8e05efb3fb754b0992d3df96cc021c137fddbd92
kgc0120 <senru0508@gmail.com>
2021-06-13 23:45:03 +0900

    jpa 연관관계 매핑 기초

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,9 @@
 package hellojpa;
 
-import org.hibernate.event.spi.SaveOrUpdateEvent;
-
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class JpaMain {
@@ -17,55 +18,28 @@ public class JpaMain {
         transaction.begin();
 
         try {
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            entityManager.persist(team);
 
             Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
             entityManager.persist(member);
 
+            entityManager.flush();
+            entityManager.clear();
 
-//            // 객체를 생선한 상태(비영속)
-//            Member member = new Member(240L, "A");
-//            Member member2 = new Member(250L, "B");
-//            Member member3 = new Member(260L, "C");
+            Member member1 = entityManager.find(Member.class, member.getId());
 
-//            entityManager.persist(member);
-//            entityManager.persist(member2);
-//            entityManager.persist(member3);
+            List<Member> members = member1.getTeam().getMembers();
+            for (Member member2 : members) {
+                System.out.println(member2.getTeam().getName());
+            }
 
-
-//            List<Member> members = entityManager.createQuery("select m from Member m", Member.class).getResultList();
-////
-//            System.out.println("=============");
-//            for (Member member1 : members) {
-//                System.out.println(member1.getName());
-//            }
 
             transaction.commit();
-
-
-//            // 객체를 저장한 상태(영속)
-//           entityManager.persist(member);
-//
-//           // 회원 엔티티를 영속성 컨텍스트에서 분리(준영속 상태)
-//           entityManager.detach(member);
-//
-//           // 객체를 삭제한 상태(삭제)
-//           entityManager.remove(member);
-
-
-//            Member member = entityManager.find(Member.class, 1L);
-//            member.setName("HelloJPA");
-            // JPQL
-            // 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
-            // 특정 데이터베이스 SQL에 의존X
-//            List<Member> resultList = entityManager.createQuery("select m from Member as m", Member.class) // 객체를 대상으로 하는 객체 지향 쿼리
-//                    .setFirstResult(5)
-//                    .setMaxResults(8)
-//                    .getResultList();
-//
-//            for (Member member : resultList) {
-//                System.out.println("memberName =  " + member.getName());
-//            }
-
         } catch (Exception e) {
             transaction.rollback();
         } finally {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,40 +1,25 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 1)
-
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
+    @Column(name = "USERNAME")
     private String username;
 
-    private Integer age;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDateTime localDateTimeTest;
-
-    @Lob
-    private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -48,55 +33,15 @@ public class Member {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUsername(String name) {
+        this.username = name;
     }
 
-    public Integer getAge() {
-        return age;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public LocalDateTime getLocalDateTimeTest() {
-        return localDateTimeTest;
-    }
-
-    public void setLocalDateTimeTest(LocalDateTime localDateTimeTest) {
-        this.localDateTimeTest = localDateTimeTest;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
